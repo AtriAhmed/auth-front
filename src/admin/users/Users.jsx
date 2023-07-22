bc16ae1da204d84c7ee162238b98a59685d9a434
AtriAhmed <atriahmed.1999@gmail.com>
2023-07-22 19:28:23 +0100

    modal

--- src/admin/users/Users.jsx
+++ src/admin/users/Users.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AllUsers from '../../users/AllUsers'
 import CreateUser from '../../users/CreateUser'
+import Modal from '../../Modal'
 
 export default function Users() {
+  const [addModalShow, setAddModalShow] = useState(false)
+
   return (
     <div>
          <AllUsers />
+         <button onClick={()=>setAddModalShow()}>Add user</button>
+         <Modal
+        show={addModalShow}
+        hide={() => {
+          setAddModalShow(false);
+        }}
+        dialogClassName="w-full sm:max-w-2xl h-fit my-auto pb-5 rounded-xl"
+      >
       <CreateUser />
+      </Modal>
       <UpdateUser />
       <DeleteUser />
     </div>
